e935f1f9558008cc4a47831803b25f3a423f05f5
Clio Brichaut <clio.brichaut@gmail.com>
2019-11-04 18:02:05 +0100

    v1.2

--- app/controllers/CProfile.php
+++ app/controllers/CProfile.php
@@ -3,3 +3,10 @@
     "Profile" (own an others) page.
     ...
 \* *********************************************************** */
+
+// Sets the output values and calls the views.
+$output->set_head_title('Profile');
+
+require_once(Config::VIEW_HEADER);
+require_once(Router::$page['view']);
+require_once(Config::VIEW_FOOTER);
